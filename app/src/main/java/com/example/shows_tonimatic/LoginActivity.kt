d4030b2edd28c0732b0b573bf3b80a5b65639bfe
matic-toni <tonimatic8@gmail.com>
2021-07-17 17:14:23 +0200

    Added constant for minimal password length, added color to text box, renamed object

--- app/src/main/java/com/example/shows_tonimatic/LoginActivity.kt
+++ app/src/main/java/com/example/shows_tonimatic/LoginActivity.kt
@@ -10,6 +10,10 @@ import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
 
+    companion object {
+        const val MIN_PASS_LENGTH = 6
+    }
+
     private lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,15 +36,15 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun initEmailAndPassword(obj: TextInputLayout) {
+    private fun initEmailAndPassword(inputLayout: TextInputLayout) {
         val emailEdit = binding.emailEdit.editText
         val passwordEdit = binding.passwordEdit.editText
 
-        obj.editText?.addTextChangedListener {
+        inputLayout.editText?.addTextChangedListener {
             val email = emailEdit?.text.toString()
             val password = passwordEdit?.text.toString()
 
-            if (email.isEmpty() || password.length < 6) {
+            if (email.isEmpty() || password.length < MIN_PASS_LENGTH) {
                 binding.loginButton.isEnabled = false
                 binding.loginButton.setBackgroundColor(Color.parseColor("#BBBBBB"))
                 binding.loginButton.setTextColor(Color.WHITE)
